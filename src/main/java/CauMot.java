b8deecc943c91c7a844cfd40c95fa4183b50db34
nguyen-deptrai <synguyen123456ck@gmail.com>
2025-03-06 11:37:50 +0700

    update new code

--- src/main/java/CauMot.java
+++ src/main/java/CauMot.java
@@ -1,3 +1,9 @@
 public class CauMot {
-
+    public static int sumArray(int[] arr) {
+        int sum = 0;
+        for (int num : arr) {
+            sum += num;
+        }
+        return sum;
+    }
 }
